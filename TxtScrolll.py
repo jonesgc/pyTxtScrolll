30735b37e3a84c9205e6e3d4bb3b58c363ec836d
greg <gregcj@hotmail.co.uk>
2019-07-14 18:22:07 +0100

    Started on Block class
    Working on block class which will work like elements of a display and light up when activated. They will each have their own batch for the time being. In the future mgiht need to put them in the same batch as the textArea for efficientcy.

--- TxtScrolll.py
+++ TxtScrolll.py
@@ -15,12 +15,46 @@ class TextArea:
         self.y = yPos
         self.batch = pyglet.graphics.Batch()
 
+        self.blockList = []
+
         #Creates a quad at 0,0,0 (the first vertex is at this location) with input variables.
-        self.batch.add(4, GL_QUADS, None, ("v2f", (x, y,  x + width, y,  x + width, y + height,  x, y + height,)), 
-        ('c3B', (255, 255, 255,255, 255, 255,255, 255, 255,255, 255, 255,)) )
+        self.batch.add(4, GL_QUADS, None, ("v2f", (self.x, self.y,  self.x + self.width, self.y,  self.x + self.width, self.y + self.height,  self.x, self.y + self.height,)), 
+        ('c3B', (255, 255, 255,255, 255, 255,255, 255, 255,255, 255, 255,)))
+
+        self.addBlocks()
+
+    #Fills the text area with blocks.
+    def addBlocks(self):
+        self.blockList.append(Block(100, 100))
+        self.drawBlocks()
+        return True
 
     def draw(self):
         self.batch.draw()
+    
+    def drawBlocks(self):
+        for i, val in enumerate(self.blockList):
+            print(i, ",", val)
+
+#The block class will be used to create letters.
+class Block:
+    def __init__(self, xPos, yPos):
+        self.x = xPos
+        self.y = yPos
+        self.state = 0
+        self.blockBatch = pyglet.graphics.Batch()
+        self.blockSize = 25
+        self.blockBatch.add(4, GL_QUADS, None, ("v2f", (self.x, self.y,   self.x + self.blockSize, self.y,   self.x + self.blockSize, self.y + self.blockSize,   self.x, self.y + self.blockSize,)),
+        ("c3B", (0,0,0, 0,0,0, 0,0,0, 0,0,0,)))
+    
+    #Switches the block on or off.
+    def flip():
+        state = ~state
+        return state
+    
+    def draw(self):
+        self.blockBatch.draw()
+
 
 #Window class for the application. Handles some basic input.
 class Window(pyglet.window.Window):
@@ -30,7 +64,7 @@ class Window(pyglet.window.Window):
         self.keys = key.KeyStateHandler()
         self.push_handlers(self.keys)
 
-        self.textArea = TextArea(100, 100, 500, 200)
+        self.textArea = TextArea(105, 105, 500, 200)
         #pyglet.clock.schedule(self.update)
 
 
