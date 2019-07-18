ef2b65ef02010df8128ab9840097d88bfb8e5450
greg <gregcj@hotmail.co.uk>
2019-07-18 23:12:52 +0100

    Block addition complete
    Added the functions to fill the text area with blocks, currently this is done using alot of hard coded values. The text area is meant to be white, but now if 250 blocks are requested it will fill with red (the current colour of the blocks). There is no spacing between blocks yet or padding around the edges.

--- TxtScrolll.py
+++ TxtScrolll.py
@@ -21,20 +21,34 @@ class TextArea:
         self.batch.add(4, GL_QUADS, None, ("v2f", (self.x, self.y,  self.x + self.width, self.y,  self.x + self.width, self.y + self.height,  self.x, self.y + self.height,)), 
         ('c3B', (255, 255, 255,255, 255, 255,255, 255, 255,255, 255, 255,)))
 
-        self.addBlocks()
+        self.addBlocks(self.x, self.y, 250, self.width, self.height)
 
-    #Fills the text area with blocks.
-    def addBlocks(self):
-        self.blockList.append(Block(100, 100))
-        self.drawBlocks()
+    #Fills the text area with blocks. StartX & startyY refer to the position of where blocks are started to be drawn from.
+    def addBlocks(self, startX, startY, numberOfblocks, maxX, maxY):
+        offsetX = 0
+        offsetY = 0
+        i = 0
+        while i < numberOfblocks:
+            self.blockList.append(Block(startX + offsetX, startY + offsetY))
+            i +=1
+            #Adding + 20 which is the current size of the block accounts for the position of the next block, this stops blocks being added past the maxium.
+            if( offsetX +20 >= maxX):
+                offsetY += 20
+                offsetX = 0
+            else:
+                offsetX += 20
+            
         return True
 
     def draw(self):
         self.batch.draw()
+        self.drawBlocks()
     
+    #Iterates through the blockList calling draw on each block, weather anything is output depends on that blocks flipped state.
     def drawBlocks(self):
         for i, val in enumerate(self.blockList):
-            print(i, ",", val)
+            val.draw()
+
 
 #The block class will be used to create letters.
 class Block:
@@ -43,17 +57,21 @@ class Block:
         self.y = yPos
         self.state = 0
         self.blockBatch = pyglet.graphics.Batch()
-        self.blockSize = 25
+        self.blockSize = 20
         self.blockBatch.add(4, GL_QUADS, None, ("v2f", (self.x, self.y,   self.x + self.blockSize, self.y,   self.x + self.blockSize, self.y + self.blockSize,   self.x, self.y + self.blockSize,)),
-        ("c3B", (0,0,0, 0,0,0, 0,0,0, 0,0,0,)))
+        ("c3B", (255,0,0, 255,0,0, 255,0,0, 255,0,0,)))
     
     #Switches the block on or off.
     def flip():
-        state = ~state
+        self.state = ~self.state
         return state
     
     def draw(self):
-        self.blockBatch.draw()
+        #This adds the other part to the functionality of the blocks, allowing them to be "flipped" on or off.
+        if(self.state == 0):
+            self.blockBatch.draw()
+        
+        
 
 
 #Window class for the application. Handles some basic input.
@@ -64,7 +82,8 @@ class Window(pyglet.window.Window):
         self.keys = key.KeyStateHandler()
         self.push_handlers(self.keys)
 
-        self.textArea = TextArea(105, 105, 500, 200)
+        #Creates a text area with these values, for the time being the values entered should adhere to a plan for the size of the blocks, however in the future it would be better if the size of the textArea defined the size of the blocks.
+        self.textArea = TextArea(100, 100, 500, 200)
         #pyglet.clock.schedule(self.update)
 
 
